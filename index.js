0c8894723e9c8a333b691a9dcc2e5a05509b4410
Jessie Guzman <jessieguzman777@gmail.com>
2022-10-31 20:30:01 -0500

    commit tree 4642c1a951f91be1cafedf2b16461a32374166e8

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// Write your code in this file!
+// // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+// const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
 const currentUser = 'Grace Hopper';
 
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser} !`;
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-
-const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
